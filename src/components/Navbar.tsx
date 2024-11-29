14852b689c2e6938ff2ae8b47869d947b70e77b2
| ｓｙａｍ | <syamnandhu3@gmail.com>
2024-11-29 22:43:09 +0530

    navbar

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <div>
+        <ul>
+            <li>Home </li>
+            <li>Connections</li>
+            <li>About</li>
+            <li>Home</li>
+            <li>Profile</li>
+        </ul>
+    </div>
+  )
+}
+
+export default Navbar
