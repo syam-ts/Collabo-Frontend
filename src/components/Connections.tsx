c499426defde625ce85f772cfd3e122763c0b822
| ｓｙａｍ | <syamnandhu3@gmail.com>
2024-12-03 23:27:45 +0530

    card finished

--- src/components/Connections.tsx
+++ src/components/Connections.tsx
@@ -5,10 +5,12 @@ const Connections = () => {
     <div className='flex gap-12 justify-center my-44'> 
         <div className='w-96 h-96 bg-slate-300 text-center'>
         <span className='font-mono'> Developer 1 </span>
+        <img src='https://media.licdn.com/dms/image/v2/D5622AQEFkvMNDm93PA/feedshare-shrink_800/feedshare-shrink_800/0/1728405591847?e=2147483647&v=beta&t=sUMZ6mQpCJVJ_qBkDi3ASRFcElJOETbuYu6WiwcQinU' />
         </div>
-        
+  <span className='my-auto font-bold bg-slate-600 rounded-md text-center text-white w-32 h-8'> Match </span>
         <div className='w-96 h-96 bg-slate-300 text-center'>
         <span className='font-mono'> Developer 2 </span>
+        <img src='https://imgcdn.stablediffusionweb.com/2024/9/13/92f952a3-8a39-4cb9-a6c6-abdd865afe19.jpg' />
         </div>
     </div>
   )
